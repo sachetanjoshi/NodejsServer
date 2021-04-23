8ad31e446116c757cec16b15b79eebebccb4ce23
Sachetanjoshi <sachetanjoshi99@gmail.com>
2021-04-23 12:48:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const fs = require('fs');
+const _ = require('lodash');
+
+const server = http.createServer((req,res)=>{
+  //console.log(req.url, req.method);
+
+  const num = _.random(2,20);
+  console.log(num);
+
+  const greet = _.once(()=>{
+    console.log('hello')
+  });
+
+  greet();
+  greet();
+  let path = './views/';
+  
+  switch(req.url)
+  {
+    case '/':
+      path += 'index.html';
+      res.statusCode=200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode=200;
+      break;
+    case '/about=me':
+      res.statusCode=301;
+      res.setHeader('Location','/about');
+      res.end();
+      break;
+    default:
+      path += '404.html'
+      res.statusCode=404;
+      break;
+  }
+  //set header
+  res.setHeader('Content-type','text/html');
+
+  fs.readFile(path, (err,data)=>{
+    if(err){
+      console.log(err);
+      res.end();
+    }
+    else{
+      res.write(data);
+      res.end();
+    }
+  })
+  
+ 
+});
+
+server.listen(3000, 'localhost',()=>{
+  console.log('listening for request on port 3000');
+});
